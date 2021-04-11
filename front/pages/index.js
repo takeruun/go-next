0cde8e391f361ce47685013d95a10a3eac9894dc
takeru.hashimoto <hstake25@gmail.com>
2021-04-12 07:35:09 +0900

    簡単なform

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,65 +1,78 @@
 import Head from 'next/head'
+import React from 'react'
+import request from 'superagent'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GO NEXT APP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="/">Go Next app</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <UserForm/>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
+}
+
+class UserForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {name: '', email: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    var name = this.state.name;
+    var email = this.state.email;
+
+    switch(event.target.name) {
+      case 'name':
+        name = event.target.value;
+        break;
+      case 'email':
+        email = event.target.value;
+        break;
+    }
+
+    this.setState({ name: name, email: email })
+  }
+
+  handleSubmit(event){
+    request
+      .post('htpp://localhost:8080/users/')
+      .set("content-type", "application/json")
+      .send({ name: this.state.name, email: this.state.email })
+      .end((err, res) => {
+        console.log(res)
+      })
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          name:
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange}></input>
+        </label>
+        <br/>
+        <label>
+          email:
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
 }
