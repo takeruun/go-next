fa9b03db3d85c99c79f4415ff160858f8e44dcf0
hashimoto <t-hashimoto@anysense.co.jp>
2021-04-12 12:01:42 +0900

    cors対策 bindFormでリクエストData取得

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module app
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jinzhu/gorm v1.9.16
--- api/main.go
+++ api/main.go
@@ -6,14 +6,15 @@ import (
   "strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
-  gorm.Model
-  name string
-  email string
+  Name string `form:"name" json:"name"`
+  Email string `form:"email" json:"email"`
+  CreatedAt time.Time `json:"created_at"`
 }
 
 func main() {
@@ -21,6 +22,30 @@ func main() {
 	defer db.Close()
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		// 許可したいHTTPメソッドの一覧
+    AllowMethods: []string{
+      "POST",
+      "GET",
+      "OPTIONS",
+      "PUT",
+      "DELETE",
+    },
+    // 許可したいHTTPリクエストヘッダの一覧
+    AllowHeaders: []string{
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Content-Length",
+      "Accept-Encoding",
+      "X-CSRF-Token",
+      "Authorization",
+    },
+    // 許可したいアクセス元の一覧
+    AllowOrigins: []string{
+      "http://localhost:3000",
+    },
+	}))
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello",
@@ -39,14 +64,19 @@ func main() {
   })
 
   r.POST("/users", func(c *gin.Context) {
+    var req User
+    c.BindJSON(&req)
+
     db := dbConnect()
-    name := c.PostForm("name")
-    email := c.PostForm("email")
+    name := req.Name
+    email := req.Email
     fmt.Println("create user " + name + " with email " + email)
-    db.Create(&User{name: name, email: email})
+    db.Create(&User{Name: name, Email: email})
     defer db.Close()
 
-    c.Redirect(302, "/")
+    c.JSON(200, gin.H{
+      "message": "作成成功",
+    })
   })
 
   r.DELETE("/users/:id", func(c *gin.Context) {
