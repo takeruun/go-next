914ea70696ab1b372397c96d4f8fd67af50dabb5
hashimoto <t-hashimoto@anysense.co.jp>
2021-04-22 00:10:25 +0900

    安定した

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-export AWS_REGION=${AWS_REGION}
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-
-SSM_ACTIVATION=$(aws ssm create-activation --default-instance-name "fargate-ssm" --iam-role "service-role/AmazonEC2RunCommandRoleForManagedInstances" --registration-limit 1 --region $AWS_REGION)
-
-export SSM_ACTIVATION_CODE=$(echo $SSM_ACTIVATION | jq -r .ActivationCode)
-export SSM_ACTIVATION_ID=$(echo $SSM_ACTIVATION | jq -r .ActivationId)
-
-amazon-ssm-agent -register -code $SSM_ACTIVATION_CODE -id $SSM_ACTIVATION_ID -region $AWS_REGION
-
+amazon-ssm-agent -register -code "${SSM_AGENT_CODE}" -id "${SSM_AGENT_ID}" -region "ap-northeast-1"
 amazon-ssm-agent & go run main.go
 
 exec "$@"
