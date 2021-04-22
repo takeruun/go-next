95b114ebc2aacf6b696adec39debf3e8d190bcdb
takeru.hashimoto <hstake25@gmail.com>
2021-04-22 18:34:24 +0900

    arnではなくname

--- terraform/ssm_activation/main.tf
+++ terraform/ssm_activation/main.tf
@@ -7,6 +7,6 @@ module "ssm_ec2_run_role" {
 
 resource "aws_ssm_activation" "foo" {
   name               = "${var.app_name}-ssm"
-  iam_role           = module.ssm_ec2_run_role.iam_role_arn
+  iam_role           = module.ssm_ec2_run_role.iam_role_name
   registration_limit = "1"
 }
