44707c27d0948b132a0715bea50914d9bbbce332
takeru.hashimoto <hstake25@gmail.com>
2021-04-24 00:12:08 +0900

    アドバンス層への変更許可ポリシー

--- terraform/ssm_activation/data.tf
+++ terraform/ssm_activation/data.tf
@@ -9,3 +9,13 @@ data "aws_iam_policy" "ssm_directory_service_access" {
 data "aws_iam_policy" "cloud_watch_agent_server_policy" {
   arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+data "aws_caller_identity" "user" {}
+
+data "template_file" "update_ssm_service_policy" {
+  template = file("ssm_activation/update_ssm_service_policy.json")
+
+  vars = {
+    account_id = local.account_id
+  }
+}
--- terraform/ssm_activation/main.tf
+++ terraform/ssm_activation/main.tf
@@ -6,7 +6,17 @@ module "ssm_ec2_run_role" {
     data.aws_iam_policy.ssm_managed_instance_core.arn,
     data.aws_iam_policy.ssm_directory_service_access.arn,
     data.aws_iam_policy.cloud_watch_agent_server_policy.arn,
+    aws_iam_policy.update_ssm_service_policy.arn,
   ]
+}
+
+locals {
+  account_id = data.aws_caller_identity.user.account_id
+}
+
+resource "aws_iam_policy" "update_ssm_service_policy" {
+  name   = "update-ssm-service"
+  policy = data.template_file.update_ssm_service_policy.rendered
 }
 
 resource "aws_ssm_activation" "this" {
